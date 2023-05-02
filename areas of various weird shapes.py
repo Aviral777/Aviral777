a8e52d162b946b4ee2f9b77eca21efd825b7ce99
Aviral Tanmay <132336284+Aviral777@users.noreply.github.com>
2023-05-02 16:38:41 +0530

    Add files via upload

---
+++ areas of various weird shapes.py
@@ -0,0 +1,23 @@
+#area of right angle triangle- 1/2*l*w
+print("Today We will find area of 4 shapes Starting with")
+print('The Area of Right Angle triangle')
+l=int(input('Enter length for triangle   =  '))
+w=int(input('Enter Breadth for triangle  =  '))
+print( 'The Area of Right Angle triangle is',1/2*(l*w))
+#Square
+print("Next Shape :","Square")
+s=int(input("Enter the Length of the side of a square  = "))
+print('The Area of Square is', s*s)
+#Parallelogram
+print("Next shape: Parallelogram")
+h=int(input("Enter Height for Parallelogram  = "))
+b=int(input("Enter base for Parallelogram    = "))
+print('The Area of Parallelogram is', h*b)
+#Rhombus
+print("Last Shape: Rhombus")
+p=int(input("Enter Diagonal 1 for Rhombus  = "))
+q=int(input("Enter Diagonal 2 for Rhombus  = "))
+print('The Area of Rhombus is' ,(p*q)/2)
+print()
+print()
+print("Thanks !")
